d802710d726ac7f1c1afba8dd2edc97685e42539
alwayswim <alwayswim@gmail.com>
2021-10-05 12:58:38 -0700

    test file added

--- test.c
+++ test.c
@@ -6,6 +6,15 @@ int main(void)
   
 _printf("Let's try to printf a simple sentence. \n");
 printf("Let's try to printf a simple sentence. \n");
-
- return (0);
+_printf("Character:[%c]\n", 'Z');
+printf("Character:[%c]\n", 'Z');
+_printf("Negative number: [%i]\n", -3003);
+printf("Negative number: [%i]\n", -3003);
+_printf("String:[%s]\n", "Habtamu and Zelalem in collaboration!");
+printf("String:[%s]\n", "Habtamu and Zelalem in collaboration!");
+_printf("Decimal number:[%d]\n", 534.55);
+printf("Decimal number:[%d]\n", -534.55);
+_printf("Percent:[%%]\n");
+printf("Percent:[%%]\n");
+return (0);
 }
